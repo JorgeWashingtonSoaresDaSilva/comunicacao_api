0079203cd04cb0fd50faae6d3d9c2b547267c486
Jorge Washington Soares da Silva <jwss486@gmail.com>
2026-07-13 21:36:40 -0300

    Integração com API Notificação

--- src/main/java/com/jwss/studio/comunicacao_api/ComunicacaoApiApplication.java
+++ src/main/java/com/jwss/studio/comunicacao_api/ComunicacaoApiApplication.java
@@ -2,7 +2,9 @@ package com.jwss.studio.comunicacao_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ComunicacaoApiApplication {
 
---
+++ src/main/java/com/jwss/studio/comunicacao_api/business/NotificacaoService.java
@@ -0,0 +1,23 @@
+package com.jwss.studio.comunicacao_api.business;
+
+
+
+import com.jwss.studio.comunicacao_api.api.dto.ComunicacaoOutDTO;
+import com.jwss.studio.comunicacao_api.infraestructure.client.NotificacaoClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class NotificacaoService {
+
+    private final NotificacaoClient notificacaoClient;
+
+
+    public void enviaComunicacaoEmail(ComunicacaoOutDTO dto) {
+
+        notificacaoClient.enviaComunicacaoEmail(dto);
+    }
+
+
+}
